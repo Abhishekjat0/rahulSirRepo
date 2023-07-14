6a2bad639369ff73833d9535d61d740a081421e9
Abhishekjat0 <138009598+Abhishekjat0@users.noreply.github.com>
2023-07-14 16:19:13 +0530

    static correct code

--- static/index.js
+++ static/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const static= path.join(__dirname,"../public");
+const static= path.join(__dirname,"./public");
 console.log(__dirname);
 console.log(static);
 app.use(express.static(static));
